2d2b415a5eca44a1db1177d2e991f7fcc35457cf
OKirsenko <senka3434@gmail.com>
2022-01-13 23:42:34 +0200

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,70 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
 
+Notiflix.Notify.init({
+  width: '350px',
+  fontSize: '24px',
+  distance: '30px',
+  clickToClose: true,
+});
+
+const inputEl = document.querySelector('#search-box');
+const countryListEl = document.querySelector('.country-list');
+const countryInfoEl = document.querySelector('.country-info');
 const DEBOUNCE_DELAY = 300;
+const API_URL = `https://restcountries.com/v3.1/name/`;
+
+inputEl.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
+
+function onInput() {
+  if (inputEl.value.length === 0) {
+    countryInfoEl.innerHTML = '';
+    countryListEl.innerHTML = '';
+    return;
+  }
+  fetchCountries(inputEl.value.trim()).then(addingToList);
+}
+
+function fetchCountries(name) {
+  return fetch(`${API_URL}${name}?fields=name,capital,population,flags,languages`).then(response =>
+    response.json(),
+  );
+}
+function addingToList(countries) {
+  if (countries.length === 1) {
+    oneCoountryMarkUp(countries);
+  } else if (countries.length > 10) {
+    Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+  } else if (countries.status === 404) {
+    Notiflix.Notify.failure('Oops, there is no country with that name');
+  } else {
+    manyCountriesMarkUp(countries);
+  }
+}
+
+function oneCoountryMarkUp(country) {
+  const markup = country.map(country => {
+    // console.log(country);
+    return `<div class="country-card">
+    <img src="${country.flags.svg}" class="main-img"> 
+    <h1>${country.name.official}</h1>
+    <ul class="char-list">
+    <li class="char-item">Capital:  ${country.capital}</li>
+    <li class="char-item">Population: ${country.population}</li>
+    <li class="char-item">Languages: ${Object.values(country.languages)}</li>
+    </ul></div>`;
+  });
+  countryInfoEl.innerHTML = markup;
+  countryListEl.innerHTML = '';
+}
+
+function manyCountriesMarkUp(countries) {
+  const markup = countries.map(
+    country =>
+      `<li class="countries-list"><img src="${country.flags.svg}" class="list-images">${country.name.official}</li>`,
+  );
+
+  countryListEl.innerHTML = markup;
+  countryInfoEl.innerHTML = '';
+}
